959c24b0bfad6c10931c6f66d9664708aaa6d737
Taah <admin@taah.dev>
2025-03-04 06:51:59 -0800

    mmm.. debug

--- src/app/api/scheduled/route.ts
+++ src/app/api/scheduled/route.ts
@@ -14,28 +14,33 @@ type Recipient = {
 };
 
 export const GET = async () => {
-  const result = (await emails()) as ({
-    recipients: Recipient[];
-    id: UUID;
-  } & Email)[];
-  for (const x of result) {
-    const scheduled = new Date(x.scheduled_date);
-    const now = new Date();
-    if (x.status === "scheduled" && scheduled <= now) {
-      const template = (await templates())?.filter(
-        (template) => template.id === x.templateId,
-      )[0];
-      const recipients = Array.from(
-        new Set(x.recipients.map((val) => val.user_email)),
-      );
-      if (!recipients || recipients.length == 0) {
-        continue;
+  try {
+    const result = (await emails()) as ({
+      recipients: Recipient[];
+      id: UUID;
+    } & Email)[];
+    for (const x of result) {
+      const scheduled = new Date(x.scheduled_date);
+      const now = new Date();
+      if (x.status === "scheduled" && scheduled <= now) {
+        const template = (await templates())?.filter(
+          (template) => template.id === x.templateId,
+        )[0];
+        const recipients = Array.from(
+          new Set(x.recipients.map((val) => val.user_email)),
+        );
+        if (!recipients || recipients.length == 0) {
+          continue;
+        }
+        await sendEmail(x.subject, template.body, recipients);
+        await updateEmail(x.id, {
+          status: "sent",
+        });
       }
-      await sendEmail(x.subject, template.body, recipients);
-      await updateEmail(x.id, {
-        status: "sent",
-      });
     }
+    return NextResponse.json({ message: result }, { status: 200 });
+
+  } catch (e) {
+    return NextResponse.json({ error: e }, { status: 400 })
   }
-  return NextResponse.json({ message: result }, { status: 200 });
 };
